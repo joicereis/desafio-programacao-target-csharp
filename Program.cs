7d78a5663028d54d124af99950cd542e04fb291f
joice.jdosreis@gmail.com <joice.jdosreis@gmail.com>
2025-02-13 11:36:42 -0300

    Adicinando Desafio1 ao repositório

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Desafio1_Soma();
+    }
+
+    private static void Desafio1_Soma()
+    {
+        int INDICE = 13, SOMA = 0, K = 0;
+        while (K < INDICE)
+        { 
+            K = K + 1;
+            SOMA = SOMA + K;
+        }
+        Console.WriteLine(SOMA);
+    }
+}
